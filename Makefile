2cf79f99124a0ea83a9b23a93fe3d3a2c18f473c
EC2 Default User <ec2-user@ip-172-31-26-206.eu-west-2.compute.internal>
2019-10-21 10:19:40 +0000

    python version changed

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 init:
-	pip install -r requirements.txt
+	python3.6 -m pip install -r requirements.txt --user
 test:
 	nosetests -verbosity=2 tests/
--- tests/test_hot_cookie.py
+++ tests/test_hot_cookie.py
@@ -5,11 +5,11 @@ from cookie import get_hot_cookie
 class BasicTestSuite(unittest.TestCase):
 
     def test_get_hot_cookie(self):
-        active_cookies = get_hot_cookie("test.csv", "2018-12-09")
+        active_cookies = get_hot_cookie("tests/test.csv", "2018-12-09")
         self.assertEqual(str(active_cookies), 'AtY0laUfhglK3lC7')
 
     def test_get_hot_cookie_multiple(self):
-        active_cookies = get_hot_cookie("test.csv", "2018-12-08")
+        active_cookies = get_hot_cookie("tests/test.csv", "2018-12-08")
         self.assertMultiLineEqual(str(active_cookies), "4sMM2LxV07bPJzwf\n SAZuXPGUrfbcn5UA\n fbcn5UAVanZf6UtG")
 
 
